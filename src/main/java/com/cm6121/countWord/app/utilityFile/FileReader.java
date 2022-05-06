15c32bec5351de2c36d860da25ba6f67ecfc1132
David Ghawi <GhawiD@cardiff.ac.uk>
2022-05-06 19:59:52 +0100

    Updated FileReader class

--- src/main/java/com/cm6121/countWord/app/utilityFile/FileReader.java
+++ src/main/java/com/cm6121/countWord/app/utilityFile/FileReader.java
@@ -11,7 +11,7 @@ import java.util.LinkedHashMap;
 public class FileReader {
     private Document document = null;
 
-    public Document readFile (File file) throws IOException {
+    public Document readFile(File file) throws IOException {
         String[] values;
         String line;
 
@@ -39,16 +39,16 @@ public class FileReader {
         wordsInFile = wordsInFile.replaceAll("—", " ");
         String[] words = wordsInFile.split("\\s+");
 
-        for (int i = 0; i < words.length; i++){
+        for (int i = 0; i < words.length; i++) {
 
             words[i] = words[i].replaceAll("[^\\w]", "");
 
             words[i] = words[i].toLowerCase();
 
-            if (numberWords.containsKey(words[i]) && words[i].length() > 1){
+            if (numberWords.containsKey(words[i]) && words[i].length() > 1) {
                 int count = numberWords.get(words[i]) + 1;
                 numberWords.put(words[i], count);
-            } else if (words[i].length() > 1){
+            } else if (words[i].length() > 1) {
                 numberWords.put(words[i], 1);
             }
         }
@@ -57,7 +57,7 @@ public class FileReader {
 
     public static void readMappedFiles(String path) throws IOException {
         File CSVFile = new File(String.valueOf(path));
-        if (CSVFile.isFile()){
+        if (CSVFile.isFile()) {
             BufferedReader CSVReader = new BufferedReader(new java.io.FileReader(path));
             String line;
 
@@ -66,9 +66,9 @@ public class FileReader {
                 System.out.println("Here are the number of words in the file: " + specificFile);
                 int numberOfLines = 0;
                 int counter = 0;
-                while (numberOfLines < 21 && (line = CSVReader.readLine()) != null){
-                    if (counter == 0){
-                        counter ++;
+                while (numberOfLines < 21 && (line = CSVReader.readLine()) != null) {
+                    if (counter == 0) {
+                        counter++;
                         continue;
                     }
                     String[] information = line.split(",");
@@ -79,8 +79,8 @@ public class FileReader {
             } else {
                 System.out.println("Here are the number of words in the file: " + specificFile);
                 int counter = 0;
-                while ((line = CSVReader.readLine()) != null){
-                    if (counter == 0){
+                while ((line = CSVReader.readLine()) != null) {
+                    if (counter == 0) {
                         counter++;
                         continue;
                     }
@@ -95,30 +95,31 @@ public class FileReader {
     public static void wordSearch(String path, String word, File file) throws IOException {
         File CSVFile = new File(String.valueOf(path));
 
-        if(CSVFile.isFile()) {
+        if (CSVFile.isFile()) {
             BufferedReader CSVReader = new BufferedReader(new java.io.FileReader(path));
             String line;
             int contains = 0;
             int linesInFile = 0;
-            while ((line = CSVReader.readLine()) != null){
-                linesInFile ++;
+            while ((line = CSVReader.readLine()) != null) {
+                linesInFile++;
                 String[] information = line.split(",");
                 String column1 = information[0];
                 String column2 = information[1];
                 if (column1.equals(word)) {
                     System.out.println("The word: '" + word + "' appears: " + column2 + " times in the file: " + file.getName());
                 } else {
-                    contains ++;
+                    contains++;
                 }
             }
 
-            if (contains == linesInFile){
+            if (contains == linesInFile) {
                 System.out.println("Unfortunately, the word '" + word + "' Does not appear in the file: " + file.getName());
             }
         }
     }
 
     static HashMap<String, Integer> completeMap = new HashMap<>();
+
     public static HashMap<String, Integer> completeMap(HashMap<String, Integer> map) {
         map.forEach((k, v) -> completeMap.merge(k, v, (v1, v2) -> v1 + v2));
 
@@ -130,5 +131,9 @@ public class FileReader {
         File file = new File(path.toString());
         WriterCSV.writeToFile(reverseCompleteMap, file);
         return reverseCompleteMap;
+    }
+
+    public static File[] getResourceFolderFiles(String folder) {
+        return new File(folder).listFiles();
     }
 }
